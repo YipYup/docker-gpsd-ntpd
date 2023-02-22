bc9db8436639c835d74b3091b311f636a48ecd3e
YipYup <YipYup@users.noreply.github.com>
2023-02-22 11:57:26 -0700

    New init file

---
+++ init.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#####
+# Backup gpsd default config file
+if [[ -f /etc/default/gpsd ]]; then
+    echo "gpsd default config file exists..."
+    echo "Now checking if backup of default gpsd file exists..."
+    if [[ ! -f /etc/default/gpsd.orig ]]; then
+        $(which cp) /etc/default/gpsd /etc/default/gpsd.orig || echo "Failed to copy /etc/default/gpsd to /etc/default/gpsd.orig" && exit 1
+    else
+        true
+    fi
+else
+    echo "GPSD default config file is missing."
+    exit 1    
+fi
+#####
+# Backup GPSD socket configuration file
+if [[ -f /usr/lib/systemd/system/gpsd.socket ]]; then
+    echo "gpsd default config file exists..."
+    echo "Now checking if backup of default gpsd file exists..."
+    if [[ ! -f /root/.gpsd.socket.bak ]]; then
+        /usr/bin/cp /usr/lib/systemd/system/gpsd.socket /root/.gpsd.socket.bak || echo "Failed to copy /usr/lib/systemd/system/gpsd.socket to /root/.gpsd.socket.bak" && exit 1
+    else
+        true
+    fi
+else
+    echo "GPSD default config file is missing."
+    exit 1    
+fi
+#####
+# Make GPSd listen on any IP address for IPv6 and IPv4
+sed -i.orig -e 's/ListenStream\=\[::1\]:2947/ListenStream\=\[::\]:2947/' -e 's/ListenStream\=127.0.0.1:2947/ListenStream\=0.0.0.0:2947/' /usr/lib/systemd/system/gpsd.socket
+#####
+# 
