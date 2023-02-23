2e50033caedf15c63303182c0a01b6d0d8686273
YipYup <YipYup@users.noreply.github.com>
2023-02-22 22:04:14 -0700

    Add new init file

--- init.bash
+++ init.bash
@@ -31,4 +31,20 @@ fi
 # Make GPSd listen on any IP address for IPv6 and IPv4
 sed -i.orig -e 's/ListenStream\=\[::1\]:2947/ListenStream\=\[::\]:2947/' -e 's/ListenStream\=127.0.0.1:2947/ListenStream\=0.0.0.0:2947/' /usr/lib/systemd/system/gpsd.socket
 #####
-# 
+# Backup NTPd service file
+if [[ -f /lib/systemd/system/ntp.service ]]; then
+    echo "ntp default config file exists..."
+    echo "Now checking if backup of default gpsd file exists..."
+    if [[ ! -f /root/.ntp.service.bak ]]; then
+        /usr/bin/cp /lib/systemd/system/ntp.service /root/.ntp.service.bak || echo "Failed to copy /lib/systemd/system/ntp.service to /root/.ntp.service.bak" && exit 1
+    else
+        true
+    fi
+else
+    echo "ntp default config file is missing."
+    exit 1    
+fi
+
+#
+# Edit NTPd service file to disable the PrivateTmp file and make it accessible.
+sed -i.orig -e 's/PrivateTmp=true/#PrivateTmp=true/' /lib/systemd/system/ntp.service
