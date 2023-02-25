10cac38bf4e80f40c389c7938cf93457b1bdf29a
YipYup <YipYup@users.noreply.github.com>
2023-02-24 23:57:50 -0700

    update logic

--- init.bash
+++ init.bash
@@ -5,28 +5,30 @@
 declare -rx DOCKER_FILES_INTERNAL_SOURCE_DIR=/usr/docker-src-rootfs/
 export DOCKER_FILES_INTERNAL_SOURCE_DIR
 ###############################################################################
-# Backup gpsd default config file
+# Backup the gpsd default config file
 ###############################################################################
 if [[ -f /etc/default/gpsd ]]; then
-    echo "gpsd default config file exists..."
-    echo "Now checking if backup of default gpsd file exists..."
-    if [[ ! -f /etc/default/gpsd.orig ]]; then
-        $(which cp) /etc/default/gpsd /etc/default/gpsd.orig || echo "Failed to copy /etc/default/gpsd to /etc/default/gpsd.orig" && exit 1
-    else
+    echo "GPSd default config file exists. We will check to see if it has been"
+    echo "backed up yet..."
+    if [[ -f /etc/default/gpsd.orig ]]; then
+        echo "The backup exists. Proceeding..."
         true
+     else
+       echo "No backup of it exists. Attempting to create a backup..."
+       $(which cp) /etc/default/gpsd /etc/default/gpsd.orig || echo "Failed to copy /etc/default/gpsd to /etc/default/gpsd.orig" && exit 1
     fi
 else
     echo "GPSD default config file is missing."
-    exit 1    
+    exit 1
 fi
 ###############################################################################
-# Backup GPSD socket configuration file
+# Backup the GPSD socket configuration file
 ###############################################################################
 if [[ -f /usr/lib/systemd/system/gpsd.socket ]]; then
-    echo "gpsd default config file exists..."
-    echo "Now checking if backup of default gpsd file exists..."
+    echo "gpsd socket config file exists..."
+    echo "Now checking if backup of gpsd socket file exists..."
     if [[ ! -f /root/.gpsd.socket.bak ]]; then
-        /usr/bin/cp /usr/lib/systemd/system/gpsd.socket /root/.gpsd.socket.bak || echo "Failed to copy /usr/lib/systemd/system/gpsd.socket to /root/.gpsd.socket.bak" && exit 1
+        $(which cp) /usr/lib/systemd/system/gpsd.socket /root/.gpsd.socket.bak || echo "Failed to copy /usr/lib/systemd/system/gpsd.socket to /root/.gpsd.socket.bak" && exit 1
     else
         true
     fi
@@ -45,7 +47,7 @@ if [[ -f /lib/systemd/system/ntp.service ]]; then
     echo "ntp default config file exists..."
     echo "Now checking if backup of default gpsd file exists..."
     if [[ ! -f /root/.ntp.service.bak ]]; then
-        /usr/bin/cp /lib/systemd/system/ntp.service /root/.ntp.service.bak || echo "Failed to copy /lib/systemd/system/ntp.service to /root/.ntp.service.bak" && exit 1
+        $(which cp) /lib/systemd/system/ntp.service /root/.ntp.service.bak || echo "Failed to copy /lib/systemd/system/ntp.service to /root/.ntp.service.bak" && exit 1
     else
         true
     fi
@@ -56,7 +58,7 @@ fi
 ###############################################################################
 # Edit NTPd service file to disable the PrivateTmp file and make it accessible.
 ###############################################################################
-sed -i.orig -e 's/PrivateTmp=true/#PrivateTmp=true/' /lib/systemd/system/ntp.service
+$(which sed) -i.orig -e 's/PrivateTmp=true/#PrivateTmp=true/' /lib/systemd/system/ntp.service
 
 ###############################################################################
 # Copy gpsd config file from source directory
