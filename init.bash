15468e7f71f4176840db573442fe3cf28bb13e19
YipYup <YipYup@users.noreply.github.com>
2023-02-26 12:49:40 -0700

    Bug fix for cp

--- init.bash
+++ init.bash
@@ -7,7 +7,7 @@ export DOCKER_FILES_INTERNAL_SOURCE_DIR
 ###############################################################################
 # copy files needed for GPSd
 cp ${DOCKER_FILES_INTERNAL_SOURCE_DIR}etc/default/gpsd /etc/default/gpsd
-cp ${DOCKER_FILES_INTERNAL_SOURCE_DIR}usr/lib/systemd/system/gpsd.socket
+cp ${DOCKER_FILES_INTERNAL_SOURCE_DIR}usr/lib/systemd/system/gpsd.socket /usr/lib/systemd/system/gpsd.socket
 # copy files needed for NTP
 cp ${DOCKER_FILES_INTERNAL_SOURCE_DIR}etc/ntp.conf /etc/ntp.conf
 cp ${DOCKER_FILES_INTERNAL_SOURCE_DIR}lib/systemd/system/ntp.service
